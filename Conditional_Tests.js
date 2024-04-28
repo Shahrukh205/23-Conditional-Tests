c46ccfc5d5940623056164bf9986b28824eeb005
Shahrukh Ali <160590321+Shahrukh205@users.noreply.github.com>
2024-04-28 19:51:58 +0500

    Add files via upload

---
+++ Conditional_Tests.js
@@ -0,0 +1,13 @@
+"use strict";
+// Write a series of conditional Tests.
+// Creating car variable.
+let car = 'subaru';
+// Test no 1.
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru');
+// Test no 2.
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+// Test no 3.
+console.log("Is car != 'Honda'? I predict True.");
+console.log(car != 'Honda');
